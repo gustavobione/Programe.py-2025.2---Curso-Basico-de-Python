a7d8f139e4acc09d21c3d9101d277db22b740fa0
gustavobione <gustavoteixeira394@gmail.com>
2025-11-27 16:45:11 -0300

    feat: Lista 3 5/10

---
+++ Lista 3 - Estruturas de Repetição/C. Intervalinho.py
@@ -0,0 +1,13 @@
+primeiro = int(input())
+segundo = int(input())
+soma = primeiro + segundo
+
+print(f"A soma dos números é: {soma}")
+print("Os números pares dentro da sequência são:")
+for pares in range(primeiro, segundo + 1):
+    if pares % 2 == 0:
+        print(pares)
+print("Os números ímpares dentro da sequência são:")
+for impares in range(primeiro, segundo + 1):
+    if impares % 2 != 0:
+        print(impares)
---
+++ Lista 3 - Estruturas de Repetição/E. Soma aí.py
@@ -0,0 +1,12 @@
+soma = 0
+quantidade = 0
+
+confirmacao = str(input())
+while confirmacao == 's' or confirmacao == 'S':
+    numero = float(input())
+    soma += numero
+    quantidade += 1
+    confirmacao = str(input())
+
+print(f"Você digitou {quantidade} números.")
+print(f"A soma final é: {soma:.2f}")
