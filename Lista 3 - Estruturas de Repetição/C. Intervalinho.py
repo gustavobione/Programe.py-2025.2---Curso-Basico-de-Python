f23fa30f5b8b9ff880dbf7eb7ea3a99834321885
Gustavo <gustavoteixeira394@gmail.com>
2025-12-04 13:05:39 -0300

    fix: Lista 3 item C

--- Lista 3 - Estruturas de Repetição/C. Intervalinho.py
+++ Lista 3 - Estruturas de Repetição/C. Intervalinho.py
@@ -3,10 +3,12 @@ segundo = int(input())
 soma = primeiro + segundo
 
 print(f"A soma dos números é: {soma}")
+print()
 print("Os números pares dentro da sequência são:")
 for pares in range(primeiro, segundo + 1):
     if pares % 2 == 0:
         print(pares)
+print()
 print("Os números ímpares dentro da sequência são:")
 for impares in range(primeiro, segundo + 1):
     if impares % 2 != 0:
